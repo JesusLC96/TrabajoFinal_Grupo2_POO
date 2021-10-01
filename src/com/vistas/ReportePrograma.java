24b162ae58eb4074f99e3cf0e3b40b5b510efdad
JesusLC96 <jesuslc96@gmail.com>
2021-10-01 02:16:08 -0500

    Version Final

---
+++ src/com/vistas/ReportePrograma.java
@@ -0,0 +1,54 @@
+package com.vistas;
+
+import com.entities.Empresa;
+import com.entities.Programa;
+
+import javax.swing.*;
+
+
+public class ReportePrograma {
+
+    private JPanel myModificarPrograma;
+    private JTextField textModPrograma;
+    private JTextField textModLinea;
+    private JTextField textModNombre;
+    private JTextField textModCantidad;
+    private JButton buttonActualizarCurso;
+    private JFrame frame = new JFrame();
+    private int index;
+    PortalProgramas portalProgramas;
+
+    private Empresa empresa;
+
+    public ReportePrograma(int indice) {
+        this.index=indice;
+        empresa = Empresa.getInstance();
+        Programa programa = empresa.getProgramas().get(index);
+        textModPrograma.setText(programa.getClass().getSimpleName());
+        textModLinea.setText(programa.getLinea().toString());
+        textModNombre.setText(programa.getNombre());
+        textModCantidad.setText(String.valueOf(programa.getCantidadMaximaCursos()));
+
+        buttonActualizarCurso.addActionListener(e ->actualizarCurso());
+
+    }
+
+    public void actualizarCurso(){
+        empresa = Empresa.getInstance();
+        empresa.actualizarPrograma(index,textModNombre.getText(),Integer.parseInt(textModCantidad.getText()));
+        portalProgramas = PortalProgramas.getInstance();
+        portalProgramas.listarPrograma();
+
+    }
+
+    public void load(){
+        this.frame.add(myModificarPrograma); // agregar panel al objeto
+        this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // detener programa al cerrar
+        this.frame.setUndecorated(false); // mostrar ventanas windows
+        this.frame.pack();  //mostrar contenido
+        this.frame.setLocationRelativeTo(null); //mostrar al centro
+        this.frame.setVisible(true);    // que sea visible
+        this.frame.setSize(650,350);
+        this.frame.setResizable(true); // bloqueo cambiar tamaño de ventana
+    }
+}
